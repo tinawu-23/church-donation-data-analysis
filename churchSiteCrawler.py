7471a02c81ac5a3f9a164ef3fd4cd1ed1379ffa3
Tina Wu <ywu6@nd.edu>
2018-09-19 17:02:46 -0400

    update all sites to new dataframe

--- churchSiteCrawler.py
+++ churchSiteCrawler.py
@@ -7,10 +7,15 @@ df = pd.read_csv('PA-Parishes.csv', nrows=20)
 df_church = df['Church']
 df_city = df['City']
 
+df_urlComplete = df
+
 for i in range(df.shape[0]):
+	if df['URL'][i]!='None':
+		continue
 	querystr = df_church[i] + " " + df_city[i]
 	for site in search(querystr, tld="com", num=3, stop=1, pause=2):
 		if 'facebook' not in site and 'wikipedia' not in site:
-			print(site)
+			df_urlComplete['URL'][i]=site
 			break
+print(df_urlComplete)
 
